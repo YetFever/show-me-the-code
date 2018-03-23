33533a53d718a2bf8a3a83901411989c903de8b7
YetFever <30425928+YetFever@users.noreply.github.com>
2018-03-23 17:34:20 +0800

    Update 0001.py

--- 0001.py
+++ 0001.py
@@ -4,8 +4,8 @@ import random
 # count:随机码数量 length:随机码长度
 def PromoCode(count,length):
     promo_code_lst = []
-    for i in range(count-1):
-        promo_code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length-1)
+    for _ in range(count-1):
+        promo_code = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(length-1)
         if promo_code not in promo_code_lst:
 			promo_code_list.append(promo_code)
     return promo_code_lst
