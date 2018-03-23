dd19237826220145dfd95fa8346ed5d47a41d233
YetFever <30425928+YetFever@users.noreply.github.com>
2018-03-23 17:36:53 +0800

    Update 0001.py

--- 0001.py
+++ 0001.py
@@ -7,5 +7,5 @@ def PromoCode(count,length):
     for i in range(count-1):
         promo_code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length-1)
         if promo_code not in promo_code_lst:
-        promo_code_list.append(promo_code)
+            promo_code_list.append(promo_code)
     return promo_code_lst
